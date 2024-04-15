b8d4aec91c690849990815e29bf7ca8e1f629d64
DCodeProg <danael.legrand@outlook.fr>
2024-04-15 16:04:32 +0200

    fix!: Refactor all hash function
    Filehash function who return multiple hash has an issue where only the first hash was correct

--- src/utils/filehash.py
+++ src/utils/filehash.py
@@ -1,28 +1,30 @@
 import hashlib
 
 
-def _get_hash(file, algorithm: str) -> str:
+def _get_hash(file_path: str, algorithm: str) -> str:
     """Returns file hash with the given algorithm
 
     Args:
-        file: File to hash
+        file_path (str): File path location
         algorithm (str): Hashing algorithm name
 
     Returns:
         str: File hash in uppercase hexadecimal format
     """
     
-    # Hashs the file
-    file_hash = hashlib.file_digest(file, algorithm)
+    # Opens the file as binary file
+    with open(file_path, 'rb') as file:
+        # Hashs the file
+        file_hash = hashlib.file_digest(file, algorithm)
+            
+        # Converts the hash to hex
+        file_hash = file_hash.hexdigest()
+            
+        # Put the hash in uppercase
+        file_hash = file_hash.upper()
         
-    # Converts the hash to hex
-    file_hash = file_hash.hexdigest()
-        
-    # Put the hash in uppercase
-    file_hash = file_hash.upper()
-    
-    # Return the hash
-    return file_hash
+        # Return the hash
+        return file_hash
 
 
 def get_sha256(file_path: str) -> tuple[str, str]:
@@ -35,13 +37,11 @@ def get_sha256(file_path: str) -> tuple[str, str]:
         tuple[str, str]: Tuple (algorithm, file hash)
     """
     
-    # Opens the file as binary file
-    with open(file_path, 'rb', buffering=0) as file:
-        # Get the file hash
-        file_hash = _get_hash(file, 'sha256')
+    # Get the file hash
+    file_hash = _get_hash(file_path, 'sha256')
         
-        # Retuns a tuple (algorithm, file hash)
-        return ('sha256', file_hash)
+    # Retuns a tuple (algorithm, file hash)
+    return ('sha256', file_hash)
     
     
 def get_md5(file_path: str) -> tuple[str, str]:
@@ -54,13 +54,11 @@ def get_md5(file_path: str) -> tuple[str, str]:
         tuple[str, str]: Tuple (algorithm, file hash)
     """
     
-    # Opens the file as binary file
-    with open(file_path, 'rb', buffering=0) as f:
-        # Get the file hash
-        file_hash = _get_hash(f, 'md5')
+    # Get the file hash
+    file_hash = _get_hash(file_path, 'md5')
         
-        # Retuns a tuple (algorithm, file hash)
-        return ('md5', file_hash)
+    # Retuns a tuple (algorithm, file hash)
+    return ('md5', file_hash)
     
     
 def get_both(file_path: str) -> list[tuple[str, str]]:
@@ -73,14 +71,11 @@ def get_both(file_path: str) -> list[tuple[str, str]]:
         list[tuple[str, str]]: List of file hashes tuple (algorithm, file hash)
     """
     
-    # Opens the file as binary file
-    with open(file_path, 'rb', buffering=0) as file:
-        
-        # Retuns a list with both tuple (algorithm, file hash)
-        return [
-            ('sha256', _get_hash(file, 'sha256')),
-            ('md5', _get_hash(file, 'md5')),
-        ]
+    # Retuns a list with both tuple (algorithm, file hash)
+    return [
+        ('sha256', _get_hash(file_path, 'sha256')),
+        ('md5', _get_hash(file_path, 'md5')),
+    ]
     
     
 def get_all(file_path: str) -> list[tuple[str, str]]:
@@ -96,13 +91,10 @@ def get_all(file_path: str) -> list[tuple[str, str]]:
     # List of all the file hashes
     hash_list: list = []
     
-    # Opens the file as binary file
-    with open(file_path, 'rb', buffering=0) as file:
-        
-        # Getting all possible file hashes
-        for algo in hashlib.algorithms_available:
-            try: hash_list.append((algo, _get_hash(file, algo)))
-            except: continue
+    # Getting all possible file hashes
+    for algo in hashlib.algorithms_available:
+        try: hash_list.append((algo, _get_hash(file_path, algo)))
+        except: continue
     
     # Sorting the list by algorithm name
     hash_list.sort(key = lambda key: key[0])
